f04f85dbbea24a0b6c23719b3e293fb55eeb8de9
Andres Gonzalez <andresg9108@gmail.com>
2019-07-10 08:52:55 -0500

    Save

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const iPort = process.env.PORT || 5000;
+
 var oExpress = require('express');
 var oApp = oExpress();
-var iPort = process.env.PORT || 5000;
 
 oApp.use(oExpress.static(__dirname + '/public'));
 
 oApp.listen(iPort, function(){
+	console.log("Port in environment variables (Node.js): "+process.env.PORT);
     console.log('Port: '+iPort);
 });
